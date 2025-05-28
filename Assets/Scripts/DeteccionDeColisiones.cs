8c24183def87a614a76507e18d24fb754ff64306
Luciano Girotti <onaicul2008@gmail.com>
2025-05-28 15:47:46 -0300

    colisiones

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto " + collision.gameObject.name);
+        Destroy(collision.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject.name + ": Choqué con " + collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
